f2e6334228c014499012de584c0692a8be30413f
YuriMel0 <yurimelo216@gmail.com>
2022-12-22 17:36:40 -0300

    Criando estrutura do projeto

---
+++ src/ArvoreAVL/ArvoreAVL.py
@@ -0,0 +1,30 @@
+"""
+    UFRJ - Universidade Federal do Rio de Janeiro
+    MAE015 - Tóp. Especiais em Engenharia de dados A: Estrutura de dados
+
+    Autores:
+        Nome: Rhuan Justo
+        DRE: 118043398
+
+        Nome: Davi RIchards
+        DRE: 119022078
+
+        Nome: Sebastião Rodrigo
+        DRE: 117099621
+
+        Nome: Yuri Ferreira Melo
+        DRE: 120081378
+"""
+
+class No:
+
+    def __init__(self) -> None:
+        pass
+
+
+
+
+class ArvoreAVL:
+
+    def __init__(self) -> None:
+        pass
---
+++ src/main.py
@@ -0,0 +1,25 @@
+"""
+    UFRJ - Universidade Federal do Rio de Janeiro
+    MAE015 - Tóp. Especiais em Engenharia de dados A: Estrutura de dados
+
+    Autores:
+        Nome: Rhuan Justo
+        DRE: 118043398
+
+        Nome: Davi RIchards
+        DRE: 119022078
+
+        Nome: Sebastião Rodrigo
+        DRE: 117099621
+
+        Nome: Yuri Ferreira Melo
+        DRE: 120081378
+"""
+
+def main() -> None:
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
