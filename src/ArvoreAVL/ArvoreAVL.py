08a62220894f27cc03eb12cc79a1a58e4ce383de
YuriMel0 <yurimelo216@gmail.com>
2022-12-26 21:18:00 -0300

    iniciando o metodo inserir da arvore avl com python

--- src/ArvoreAVL/ArvoreAVL.py
+++ src/ArvoreAVL/ArvoreAVL.py
@@ -18,13 +18,32 @@
 
 class No:
 
-    def __init__(self) -> None:
-        pass
-
-
-
+    def __init__(self, key: int) -> None:
+        self.key = key
+        self.filhoEsquerda = None    
+        self.filhoDireita = None
+        self.altura = 1
 
 class ArvoreAVL:
 
     def __init__(self) -> None:
+        self.raiz = No(None)
+
+
+    def inserir(self, key: int) -> None:
+        novo = No(key)
+        if self.raiz == None:
+            self.raiz = novo
+        elif key < self.raiz.key:
+            self.raiz.filhoEsquerda = self.inserir(self.raiz.filhoEsquerda, key)
+        else:
+            self.raiz.filhoDireita = self.inserir(self.raiz.filhoDireita, key)
+
+    def getAltura(self) -> None:
+        pass
+
+    def rotacaoEsquerda(self) -> None:
+        pass
+
+    def rotacaoDireita(self) -> None:
         pass
